4ec4416c7c3c19ae8f508005934a32f36cb82b5a
wuxifei <wuxifei@finupgroup.com>
2019-04-16 17:20:34 +0800

    add postPageInfo in background.js

--- js/background.js
+++ js/background.js
@@ -1,6 +1,8 @@
 class Utils {
+  constructor() {
+    this.host = 'http://172.16.124.117:3000';
+  }
   sendData() {
-
     var xhr = new XMLHttpRequest();
     var data = new FormData();
 
@@ -31,8 +33,32 @@ class Utils {
       };
     }
   }
-}
 
+  postPageInfo(pageInfo) {
+    var xhr = new XMLHttpRequest();
+    var data = new FormData();
+    data.append("url", pageInfo.url);
+    data.append("title", pageInfo.title);
+    data.append("message", pageInfo.content);
+    xhr.open('POST', `${this.host}/api/post/page-info`, true);
+    xhr.send(data);
+    xhr.onreadystatechange = function(){
+      if(xhr.readyState === 4 && xhr.status === 200) {
+        console.log(xhr.responseText);
+        // resp = JSON.parse(xhr.responseText);
+        // code = resp.code;
+        // if (code == "2001") {
+        //   console.log(resp);
+        //   key = resp.key;
+        //   iv = resp.iv;
+        //   port.postMessage({"key": key, "iv": iv});
+        //   chrome.storage.local.set({"secretkey": key, "secretiv": iv});
+        // }
+      }
+    };
+  }
+}
+const utils = new Utils();
 
 chrome.runtime.onConnect.addListener(function(port) {
   var tab = port.sender.tab;
@@ -43,5 +69,6 @@ chrome.runtime.onConnect.addListener(function(port) {
 
   port.onMessage.addListener(function(pageInfo) {
     console.log(pageInfo);
+    utils.postPageInfo(pageInfo);
   });
 });
