63a79b1db8f28520e5012732da49aabf5c5e4a98
wuxifei <wuxifei@finupgroup.com>
2019-05-08 13:24:04 +0800

    add function this.getIndentity in background.js

--- js/background.js
+++ js/background.js
@@ -4,10 +4,86 @@
 const utils = new Utils();
 const net = new Net();
 const tabs = new Tabs();
+const storage = new Storage();
 
 class Helper {
   constructor() {
     this.communication();
+    this.serviceConfig = {};
+    this.init();
+  }
+  async init() {
+    this.getIndentity = await this.getIndentityGen();
+  }
+
+  // generate function getIndentity(use closure to avoid global pollution)
+  async getIndentityGen() {
+    var indentity = await storage.getData('indentity');
+    // make sure all prop of indentity is exist
+    if (!indentity || ![
+        'uuid',
+        'ip',
+      ].every(prop => {
+        return utils.propExists(indentity, prop);
+      })
+    ) {
+      indentity = {
+        uuid: utils.getUid(),
+        ip: await utils.getLocalIPList()
+      }
+      await storage.setData({
+        indentity
+      });
+    }
+    var count = 0;
+
+    return async function() {
+      var isChanged = false;
+      const start = Date.now();
+      // check ip per * call
+      if (count++ % 8 === 0) {
+        var ip = await utils.getLocalIPList();
+        if (!utils.theSame(ip, indentity.ip)) {
+          indentity.ip = ip;
+          isChanged = true;
+        }
+      }
+      if (isChanged) {
+        await storage.setData({
+          indentity
+        });
+      }
+      console.log(Date.now() - start);
+      return indentity;
+    }
+  }
+
+  async getServiceConfig() {
+    function isValidConfig(config) {
+      return [
+        'count_config',
+        'version',
+        'system_config.secret_key',
+        'system_config.secret_iv',
+        'system_config.whitelist',
+        'system_config.blacklist',
+      ].every(prop => {
+        return utils.propExists(config, prop);
+      })
+    }
+    if (!isValidConfig(this.serviceConfig)) {
+      this.serviceConfig = net.request(net.URL_LIST.get_config);
+    }
+    return this.serviceConfig;
+  }
+
+  async handleVisitHistory(tab) {
+    const config = await this.getServiceConfig();
+    const indentity = await this.getIndentity();
+    console.log(indentity);
+  }
+  async handlePageContent() {
+
   }
 
   async communication() {
@@ -29,6 +105,13 @@ class Helper {
         case 'send-page-content':
           console.log('send-page-content');
           console.log(request);
+          var start = Date.now();
+          console.log(md5(request.data));
+          var key  = "us5N0PxHAWuIgb0/Qc2sh5OdWBbXGady";
+          var iv   = "zAvR2NI87bBx746n";
+          var encrypted = utils.encrypt(request.data, key, iv);
+          console.log(encrypted);
+          console.log(Date.now() - start);
           break;
       }
       sendResponse(tab);
@@ -83,6 +166,7 @@ class Helper {
       if (!tab || !tab.url ||  !/^[http|https]/.test(tab.url)) {
         return;
       }
+      this.handleVisitHistory(tab);
       try {
         if (await isConnected(tabId, tab)) {
           requestPageContent(tabId);
